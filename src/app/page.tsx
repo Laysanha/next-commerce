fc781a3ddf899022bc2b74aad56b7079b17ca5af
Laysa Santiago Alfredo <laysatb78@gmail.com>
2023-12-07 11:58:40 -0300

    Implementado API de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,28 @@
-import Image from 'next/image'
+import { ProductType } from "./types/ProductType"
 
-export default function Home() {
+async function getProduts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProduts()
+  console.log(products)
+
   return (
     <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl: gap-6'>
-        <div 
-          className='bg-white rounded-md h-12 flex justify-center items-center'
-        >
-          Prod 1
-        </div>
-        <div 
-          className='bg-white rounded-md h-12 flex justify-center items-center'
-        >
-          Prod 2
-        </div>
-        <div 
-          className='bg-white rounded-md h-12 flex justify-center items-center'
-        >
-          Prod 3
-        </div>
-        <div 
-          className='bg-white rounded-md h-12 flex justify-center items-center'
-        >
-          Prod 4
-        </div>
-        <div 
-          className='bg-white rounded-md h-12 flex justify-center items-center'
-        >
-          Prod 5
-        </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'
+      >
+        {products.map((product: ProductType) => (
+          <div key={product.id} className="">
+            {product.title}
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/app/types/ProductType.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+    id: string;
+    title: string;
+    price: number | null;
+    description: string | null;
+    category: string;
+    image: string;
+    rating: string;
+}
